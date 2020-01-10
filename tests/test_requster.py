549b2babc6f5ae0e30dc9c95f1e74d672c3a4800
x <>
2020-01-10 23:32:26 +0900

    유닛테스트 - requester requests_with_errinfo 함수 테스트 추가

--- tests/test_requster.py
+++ tests/test_requster.py
@@ -12,15 +12,48 @@ class RequesterTest(unittest.TestCase):
         requster = Requster()
         res = requster.requests("http://example.com")
         
-        self.assertRegexpMatches(res.text, success_res)
+        self.assertRegex(res.text, success_res)
 
 
         print("[*] test: requests fail case")
+        
+        res = None
 
         requster = Requster()
-        res = requster.requests("http://this_is_not_exist_domain")
+        res = requster.requests("http://this_is_not_exist_domain.com")
         self.assertEqual(res, None)
-    
+
+
+    def test_requests_with_errinfo(self):
+
+        print("[*] test: requests_with_errinfo sucess case")
+
+        success_res = """<title>Example Domain</title>"""
+
+        requster = Requster()
+        res = requster.requests_with_errinfo("http://example.com")
+        
+        self.assertRegex(res.text, success_res)
+
+
+        print("[*] test: requests_with_errinfo fail case")
+        
+        res = None
+
+        requster = Requster()
+        res = requster.requests_with_errinfo("https://github.com/this_is_not_exist_domain")
+
+        self.assertGreaterEqual(res.status_code, 400)
+
+
+        print("[*] test: requests_with_errinfo error case")
+
+        res = None
+
+        requster = Requster()
+        
+        self.assertRaises(Exception, requster.requests_with_errinfo, "http://this_is_not_exist_domain.com")
+        
 
 if __name__ == '__main__':
     sys.path.insert(0, '../')
