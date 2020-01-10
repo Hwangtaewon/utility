79d7b582e391e563dda27909feb811eaef298afa
x <>
2020-01-10 17:03:43 +0900

    db 함수 파라미터 타입 에러 핸들링

--- db.py
+++ db.py
@@ -11,16 +11,44 @@ class DB(object):
         return json.loads(res.text)
 
     def get_subdomain(self, owner):
+        
+        # parameter error handling
+        if not isinstance(owner, str):
+            print("[!] Error: parameter type error - owner must be str")
+            return None
+        
         res = requester(self.dbdomain + "/subdomains/" + owner)
         return json.loads(res.text)
 
     def post_all_subdomains(self, subdomains):
+        
+        # parameter error handling
+        if not isinstance(subdomains, dict):
+            print("[!] Error: parameter type error - subdomains must be dict")
+            return None
+        for owner, subdomain in subdomains.items():
+            if not isinstance(owner, str):
+                print("[!] Error: parameter type error - key(owner) must be str")
+                return None          
+            if not isinstance(subdomain, list):
+                print("[!] Error: parameter type error - value(each owner's subdomains) must be list")
+                return None
+        
         data = json.dumps(subdomains)
-        print(data)
         res = requester(self.dbdomain + "/subdomains", method="POST", data=data)
-        return res        
+        return res
 
     def post_subdomain(self, owner, subdomains):
+
+        # parameter error handling
+        if not isinstance(owner, str):
+            print("[!] Error: parameter type error - owner must be str")
+            return None
+        if not isinstance(subdomains, list):
+            print("[!] Error: parameter type error - owner's subdomains must be list")
+            return None
+
         data = json.dumps(subdomains)
         res = requester(self.dbdomain + "/subdomains/" + owner, method="POST", data=data)
         return res
+
