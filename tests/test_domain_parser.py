47fed5ca92a80483b506827ae379d50cb1f6e7b4
x <>
2020-01-14 03:22:53 +0900

    유닛테스트 - domain_parser 추가

--- tests/test_domain_parser.py
+++ tests/test_domain_parser.py
@@ -2,7 +2,28 @@ import sys
 import unittest
 
 class DomainParserTest(unittest.TestCase):
-    
+
+    # def test_find_longest_suffix(self):
+
+    #     print("[*] test: find_longest_suffix sucess case")
+
+    #     test_case = {
+    #         "https://www.naver.com":"com",
+    #         "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":"net",
+    #         "https://logins.daum.net/accounts/signinform.do":"net",
+    #         "http://192.168.0.2/chatting/index.php":["http://192.168.0.2"],
+    #         "https://www.istarbucks.co.kr/index.do":"co.kr",
+    #         "https://www.naver.kr":"kr",
+    #         "https://www.naver.co.kr":"co.kr",
+    #         }
+
+    #     domain_parser = DomainParser()
+
+    #     for url, sucess_res in test_case.items():
+    #         result = domain_parser.find_longest_suffix(url)
+    #         self.assertEqual(result, sucess_res)
+
+
     def test_get_core_keyword(self):
 
         print("[*] test: get_core_keyword sucess case")
@@ -35,6 +56,28 @@ class DomainParserTest(unittest.TestCase):
             result = domain_parser.get_core_keyword(url,suffix)
             self.assertEqual(result, sucess_res)
 
+    def test_get_current_path(self):
+        
+        print("[*] test: get_current_path sucess case")
+
+        test_case = {
+            "https://www.naver.com":["https://www.naver.com"],
+            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["https://logins.daum.net/accounts"],
+            "https://logins.daum.net/accounts/signinform.do":["https://logins.daum.net/accounts"],
+            "http://localhost/chatting/index.php":["http://localhost/chatting"],
+            "http://192.168.0.2/chatting/index.php":["http://192.168.0.2/chatting"],
+            "https://www.istarbucks.co.kr/index.do":["https://www.istarbucks.co.kr"],
+            "https://logins.daum.net/accounts/test/signinform.do?url=https":["https://logins.daum.net/accounts/test"]
+            }
+         
+        domain_parser = DomainParser()
+
+        for url, sucess_res in test_case.items():
+            result = domain_parser.get_current_path(url)
+            self.assertEqual(result, sucess_res)
+
+
+
     def test_get_root_domain(self):
 
         print("[*] test: get_root_domain sucess case")
@@ -52,8 +95,18 @@ class DomainParserTest(unittest.TestCase):
 
         for url, sucess_res in test_case.items():
             suffix = domain_parser.find_longest_suffix(url)
-            print(suffix)
             result = domain_parser.get_root_domain(url,suffix)
+            self.assertEqual(result, sucess_res)
+
+        print("[*] test: get_core_keyword fail case")
+        
+        test_case = {
+            "http://localhost/chatting/index.php":[],
+        }
+
+        for url, sucess_res in test_case.items():
+            suffix = domain_parser.find_longest_suffix(url)
+            result = domain_parser.get_core_keyword(url,suffix)
             self.assertEqual(result, sucess_res)
 
 
