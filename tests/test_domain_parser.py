698094ce89ce83a3d45aa7b9f39e0a81d6f3c0d8
x <>
2020-01-15 08:17:04 +0900

    유닛테스트 - domain parser 리턴 타입 변경에 따른 변경

--- tests/test_domain_parser.py
+++ tests/test_domain_parser.py
@@ -29,12 +29,12 @@ class DomainParserTest(unittest.TestCase):
         print("[*] test: get_core_keyword sucess case")
 
         test_case = {
-            "https://www.naver.com":["naver"],
-            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["daum"],
-            "https://logins.daum.net/accounts/signinform.do":["daum"],
-            "http://192.168.0.2/chatting/index.php":["192.168.0.2"],
-            "https://www.istarbucks.co.kr/index.do":["istarbucks"],
-            "https://logins.daum.net/accounts/signinform.do?url=https":["daum"]
+            "https://www.naver.com":"naver",
+            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":"daum",
+            "https://logins.daum.net/accounts/signinform.do":"daum",
+            "http://192.168.0.2/chatting/index.php":"192.168.0.2",
+            "https://www.istarbucks.co.kr/index.do":"istarbucks",
+            "https://logins.daum.net/accounts/signinform.do?url=https":"daum"
             }
 
         domain_parser = DomainParser()
@@ -61,13 +61,13 @@ class DomainParserTest(unittest.TestCase):
         print("[*] test: get_current_path sucess case")
 
         test_case = {
-            "https://www.naver.com":["https://www.naver.com"],
-            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["https://logins.daum.net/accounts"],
-            "https://logins.daum.net/accounts/signinform.do":["https://logins.daum.net/accounts"],
-            "http://localhost/chatting/index.php":["http://localhost/chatting"],
-            "http://192.168.0.2/chatting/index.php":["http://192.168.0.2/chatting"],
-            "https://www.istarbucks.co.kr/index.do":["https://www.istarbucks.co.kr"],
-            "https://logins.daum.net/accounts/test/signinform.do?url=https":["https://logins.daum.net/accounts/test"]
+            "https://www.naver.com":"https://www.naver.com",
+            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":"https://logins.daum.net/accounts",
+            "https://logins.daum.net/accounts/signinform.do":"https://logins.daum.net/accounts",
+            "http://localhost/chatting/index.php":"http://localhost/chatting",
+            "http://192.168.0.2/chatting/index.php":"http://192.168.0.2/chatting",
+            "https://www.istarbucks.co.kr/index.do":"https://www.istarbucks.co.kr",
+            "https://logins.daum.net/accounts/test/signinform.do?url=https":"https://logins.daum.net/accounts/test"
             }
          
         domain_parser = DomainParser()
@@ -82,13 +82,13 @@ class DomainParserTest(unittest.TestCase):
         print("[*] test: get_domain_name sucess case")
 
         test_case = {
-            "https://www.naver.com":["www.naver.com"],
-            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["logins.daum.net"],
-            "https://logins.daum.net/accounts/signinform.do":["logins.daum.net"],
-            "http://localhost/chatting/index.php":["localhost"],
-            "http://192.168.0.2/chatting/index.php":["192.168.0.2"],
-            "https://www.istarbucks.co.kr/index.do":["www.istarbucks.co.kr"],
-            "https://logins.daum.net/accounts/test/signinform.do?url=https":["logins.daum.net"]
+            "https://www.naver.com":"www.naver.com",
+            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":"logins.daum.net",
+            "https://logins.daum.net/accounts/signinform.do":"logins.daum.net",
+            "http://localhost/chatting/index.php":"localhost",
+            "http://192.168.0.2/chatting/index.php":"192.168.0.2",
+            "https://www.istarbucks.co.kr/index.do":"www.istarbucks.co.kr",
+            "https://logins.daum.net/accounts/test/signinform.do?url=https":"logins.daum.net"
             }
 
         domain_parser = DomainParser()
@@ -98,41 +98,18 @@ class DomainParserTest(unittest.TestCase):
             result = domain_parser.get_domain_name(url, suffix)
             self.assertEqual(result, sucess_res)
 
-
-    def test_get_fileless_url(self):
-
-        print("[*] test: get_fileless_url sucess case")
-
-        test_case = {
-            "https://www.naver.com":["https://www.naver.com"],
-            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["https://logins.daum.net/accounts"],
-            "https://logins.daum.net/accounts/signinform.do":["https://logins.daum.net/accounts"],
-            "http://localhost/chatting/index.php":["http://localhost/chatting"],
-            "http://192.168.0.2/chatting/index.php":["http://192.168.0.2/chatting"],
-            "https://www.istarbucks.co.kr/index.do":["https://www.istarbucks.co.kr"],
-            "https://logins.daum.net/accounts/test/signinform.do?url=https":["https://logins.daum.net/accounts/test"]
-            }
-
-        domain_parser = DomainParser()
-
-        for url, sucess_res in test_case.items():
-            suffix = domain_parser.find_longest_suffix(url)
-            result = domain_parser.get_fileless_url(url, suffix)
-            self.assertEqual(result, sucess_res)
-
-
     def test_get_pathless_url(self):
 
         print("[*] test: pathless_url sucess case")
 
         test_case = {
-            "https://www.naver.com":["https://www.naver.com"],
-            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["https://logins.daum.net"],
-            "https://logins.daum.net/accounts/signinform.do":["https://logins.daum.net"],
-            "http://localhost/chatting/index.php":[],
-            "http://192.168.0.2/chatting/index.php":["http://192.168.0.2"],
-            "https://www.istarbucks.co.kr/index.do":["https://www.istarbucks.co.kr"],
-            "https://logins.daum.net/accounts/test/signinform.do?url=https":["https://logins.daum.net"]
+            "https://www.naver.com":"https://www.naver.com",
+            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":"https://logins.daum.net",
+            "https://logins.daum.net/accounts/signinform.do":"https://logins.daum.net",
+            "http://localhost/chatting/index.php":None,
+            "http://192.168.0.2/chatting/index.php":"http://192.168.0.2",
+            "https://www.istarbucks.co.kr/index.do":"https://www.istarbucks.co.kr",
+            "https://logins.daum.net/accounts/test/signinform.do?url=https":"https://logins.daum.net"
             }
 
         domain_parser = DomainParser()
@@ -146,12 +123,12 @@ class DomainParserTest(unittest.TestCase):
         print("[*] test: get_root_domain sucess case")
 
         test_case = {
-            "https://www.naver.com":["naver.com"],
-            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":["daum.net"],
-            "https://logins.daum.net/accounts/signinform.do":["daum.net"],
-            "http://192.168.0.2/chatting/index.php":["192.168.0.2"],
-            "https://www.istarbucks.co.kr/index.do":["istarbucks.co.kr"],
-            "https://logins.daum.net/accounts/signinform.do?url=https":["daum.net"]
+            "https://www.naver.com":"naver.com",
+            "https://logins.daum.net/accounts/signinform.do?url=https%3A%2F%2Fwww.daum.net%2F":"daum.net",
+            "https://logins.daum.net/accounts/signinform.do":"daum.net",
+            "http://192.168.0.2/chatting/index.php":"192.168.0.2",
+            "https://www.istarbucks.co.kr/index.do":"istarbucks.co.kr",
+            "https://logins.daum.net/accounts/signinform.do?url=https":"daum.net"
             }
 
         domain_parser = DomainParser()
@@ -164,7 +141,7 @@ class DomainParserTest(unittest.TestCase):
         print("[*] test: get_root_domain fail case")
         
         test_case = {
-            "http://localhost/chatting/index.php":[],
+            "http://localhost/chatting/index.php":None,
         }
 
         for url, sucess_res in test_case.items():
