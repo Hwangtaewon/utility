5ba1aca6d247bf456cf501a52080789a594ebf27
x <>
2020-01-14 23:19:07 +0900

    __init__.py 업데이트

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,5 +2,5 @@ from .db import DB
 from .dnsquery import Dnsquery
 from .domain_parser import DomainParser
 from .requester import Requester
-from .searchengine import SearchEengine
+from .searchengine import Google, Bing
 from .whois import Whois
